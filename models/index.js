a30a409d4b8656c51f25d46b8023770d7cd389e5
edgardopina <edgardopina@yahoo.com>
2021-11-07 01:17:46 -0600

    Added Comment, and Vote models and their associations in index.js

--- models/index.js
+++ models/index.js
@@ -1,4 +1,78 @@
 const User = require('./User');
 const Post = require('./Post');
+const Vote = require('./Vote');
+const Comment = require('./Comment');
 
-module.exports = { User, Post };
+
+//! REMEMBER: EACH TIME WE UPDATE THE RELATIONSHIPS (ASSOCIATIONS) between the tables, we need to use 
+//! sequelize.sync({ force: true }) in server.js to drop the tables and recreate them!
+
+User.hasMany(Post, {
+   foreignKey: 'user_id',
+});
+
+//! AND WE ALSO NEED TO MAKE THE REVERSE ASSOCIATION: a post can belong to one user but not many iusers
+Post.belongsTo(User, {
+   foreignKey: 'user_id',
+   onDelete: 'SET NULL',
+});
+
+User.belongsToMany(Post, {
+   through: Vote,
+   as: 'voted_posts',
+   foreignKey: 'user_id',
+   onDelete: 'SET NULL',
+});
+
+Post.belongsToMany(User, {
+   through: Vote,
+   as: 'voted_posts',
+   foreignKey: 'post_id',
+   onDelete: 'SET NULL',
+});
+
+Vote.belongsTo(User, {
+   foreignKey: 'user_id',
+   onDelete: 'SET NULL',
+});
+
+// ONE-TO-ONE
+Vote.belongsTo(Post, {
+   foreignKey: 'post_id',
+   onDelete: 'SET NULL',
+});
+
+// By also creating one-to-many associations directly between these models, we can perform aggregated SQL
+// functions between models.In this case, we'll see a total count of votes for a single post when queried.
+// This would be difficult if we hadn't directly associated the Vote model with the other two.
+
+User.hasMany(Vote, {
+   foreignKey: 'user_id',
+});
+
+// ONE-TO-MANY
+Post.hasMany(Vote, {
+   foreignKey: 'post_id',
+});
+
+
+Comment.belongsTo(User, {
+   foreignKey: 'user_id',
+   onDelete: 'SET NULL',
+});
+
+Comment.belongsTo(Post, {
+   foreignKey: 'post_id',
+   onDelete: 'SET NULL',
+});
+
+User.hasMany(Comment, {
+   foreignKey: 'user_id',
+   onDelete: 'SET NULL',
+});
+
+Post.hasMany(Comment, {
+   foreignKey: 'post_id',
+});
+
+module.exports = { User, Post, Vote, Comment };
