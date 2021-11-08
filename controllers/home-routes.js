26f4cb132be2223c8ca96cd8215ae56ba369c9b3
edgardopina <edgardopina@yahoo.com>
2021-11-08 00:51:06 -0700

    Created homepage.hbs using hbs syntax; completed and teste '/' route of home-routes.js

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -2,56 +2,56 @@
 //! This file will contain all of the user-facing routes, such as the homepage and login page
 //! *****************************************************************************************
 const router = require('express').Router();
-// const sequelize = require('../config/connection');
-// const { Post, User, Comment } = require('../models');
-
-// // Previously, we used res.send() or res.sendFile() for the response. Because we've hooked up a template
-// // engine, we can now use res.render() and specify which template we want to use.
-
-
+const sequelize = require('../config/connection');
+const { Post, User, Comment } = require('../models');
 
 // In this case, we want to
 // // render the homepage.handlebars template (the.handlebars extension is implied). This template was light
 // // on content; it only included a single < div >. Handlebars.js will automatically feed that into the
 // // main.handlebars template, however, and respond with a complete HTML file.
 router.get('/', (req, res) => {
-   res.render('homepage');
-//    Post.findAll({
-//       attributes: [
-//          'id',
-//          'post_url',
-//          'title',
-//          'created_at',
-//          [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count'],
-//       ],
-//       include: [
-//          {
-//             model: Comment,
-//             attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
-//             include: {
-//                model: User,
-//                attributes: ['username'],
-//             },
-//          },
-//          {
-//             model: User,
-//             attributes: ['username'],
-//          },
-//       ],
-//    })
-//       .then(dbPostData => {
-//          // posts - full array of posts data only
-//          const posts = dbPostData.map(post => post.get({ plain: true }));
-//          // we wrap the array posts and pass it as an objectto be able to add more properties to the template later
-//          res.render('homepage', {
-//             posts,
-//             loggedIn: req.session.loggedIn,
-//          });
-//       })
-//       .catch(err => {
-//          console.error(err);
-//          res.status(500).json(err);
-//       });
+   //* Previously, we used res.send() or res.sendFile() for the response. Because we've hooked up a template
+   //* engine, we can now use res.render() and specify which template we want to use.
+   //* res.render('homepage');
+
+   Post.findAll({
+      attributes: [
+         'id',
+         'post_url',
+         'title',
+         'created_at',
+         [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count'],
+      ],
+      include: [
+         {
+            model: Comment,
+            attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+            include: {
+               model: User,
+               attributes: ['username'],
+            },
+         },
+         {
+            model: User,
+            attributes: ['username'],
+         },
+      ],
+   })
+      .then(dbPostData => {
+         // .get({ plain:true }) - serialize data and removes unnecessary attributes
+         const posts = dbPostData.map(post => post.get({ plain: true })); // posts - full array of posts data
+         
+         //! wrap the array posts and pass it as an object to be able to add more properties to the template later
+         res.render('homepage', { posts });
+         //         res.render('homepage', {
+         //             posts,
+         //             loggedIn: req.session.loggedIn,
+         //          });
+      })
+      .catch(err => {
+         console.error(err);
+         res.status(500).json(err);
+      });
 });
 
 // // renders login page
