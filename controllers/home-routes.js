e739b60d280bf0e76b6e78e9df4239927f64c130
edgardopina <edgardopina@yahoo.com>
2021-11-09 14:20:04 -0700

    Created single-post.hbs template; added router.get('/post/:id')nroute to home-routes.js to render single-post.hbs

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -59,51 +59,51 @@ router.get('/login', (req, res) => {
    res.render('login'); // we do not need any variables to pass as variables in 2nd parameter
 });
 
-// // GET one post
-// router.get('/post/:id', (req, res) => {
-//    Post.findOne({
-//       where: {
-//          id: req.params.id,
-//       },
-//       attributes: [
-//          'id',
-//          'post_url',
-//          'title',
-//          'created_at',
-//          [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count'],
-//       ],
-//       include: [
-//          {
-//             model: Comment,
-//             attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
-//             include: {
-//                model: User,
-//                attributes: ['username'],
-//             },
-//          },
-//          {
-//             model: User,
-//             attributes: ['username'],
-//          },
-//       ],
-//    })
-//       .then(dbPostData => {
-//          if (!dbPostData) {
-//             res.status(404).json({ message: 'No post found with this id' });
-//             return;
-//          }
+// GET one post
+router.get('/post/:id', (req, res) => {
+   Post.findOne({
+      where: {
+         id: req.params.id,
+      },
+      attributes: [
+         'id',
+         'post_url',
+         'title',
+         'created_at',
+         [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count'],
+      ],
+      include: [
+         {
+            model: Comment,
+            attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+            include: {
+               model: User,
+               attributes: ['username'],
+            },
+         },
+         {
+            model: User,
+            attributes: ['username'],
+         },
+      ],
+   })
+      .then(dbPostData => {
+         if (!dbPostData) {
+            res.status(404).json({ message: 'No post found with this id' });
+            return;
+         }
 
-//          const post = dbPostData.get({ plain: true }); // serialize the data
-//          // pass data to template; loggedIN session variable to control what elements render and which ones not
-//          res.render('single-post', {
-//             post,
-//             loggedIn: req.session.loggedIn,
-//          });
-//       })
-//       .catch(err => {
-//          console.log(err);
-//          res.status(500).json(err);
-//       });
-// });
+         const post = dbPostData.get({ plain: true }); // serialize the data
+         // pass data to template; loggedIN session variable to control what elements render and which ones not
+         res.render('single-post', { post });
+         //    post,
+         //    loggedIn: req.session.loggedIn,
+         // });
+      })
+      .catch(err => {
+         console.log(err);
+         res.status(500).json(err);
+      });
+});
 
 module.exports = router;
